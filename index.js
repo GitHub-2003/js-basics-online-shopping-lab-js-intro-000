87630ab7049b6b223ce895a8a831e221cd14fa6e
MJU <marie.ju2012@outlook.com>
2020-03-21 04:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   var msg = "Sorry, we don't have a credit card on file for you."
-  if (cardNumber !== '') {
+  if (cardNumber !== '' or typeof cardNumber !== 'undefined') {
     msg = `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`
     cart.length = 0
   }
