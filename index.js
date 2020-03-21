7e121572c8ee0f8ff10dbcbf4cd076e51518509d
MJU <marie.ju2012@outlook.com>
2020-03-21 03:35:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ function setCart(c) {
   return cart;
 }
 
+// accepts one argument, the name of an item
+// create an "item" object consist of two key-value pairs:  itemName, itemPrice
+// the price of each item is a randomly-generated integer between 1 and 100.
 function addToCart(item) {
- // write your code here
+  var anItem = new Object({'itemName':  item, 'itemPrice': Math.floor((Math.random() * 100) + 1)})
+  cart.push(newItem)
 }
 
 function viewCart() {
